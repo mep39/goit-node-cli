e17151c495443906cb73e59b1dfda6634922935d
mepinit <evgenmep@gmail.com>
2024-04-21 09:57:30 +0300

    1+2 task

--- contacts.js
+++ contacts.js
@@ -1,19 +1,28 @@
 const fs = require("fs/promises");
-const path = require ("path");
-const contactsPath = ;
- 
+const path = require("path");
+
+const contactsPath = path.join(db, "contacts.json");
+
 async function listContacts() {
-    // ...твій код. Повертає масив контактів.
-  }
-  
-  async function getContactById(contactId) {
-    // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-  }
-  
-  async function removeContact(contactId) {
-    // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-  }
-  
-  async function addContact(name, email, phone) {
-    // ...твій код. Повертає об'єкт доданого контакту (з id).
-  }
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
